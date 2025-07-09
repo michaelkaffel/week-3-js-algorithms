876b7582de451441353cdee8429837661c9fe1e0
michaelkaffel <michaeldkaffel@gmail.com>
2025-07-08 22:01:29 -0700

    updates to test.js

--- test.js
+++ test.js
@@ -320,3 +320,45 @@ function isLess(a, b) {
 
 console.log(isLess(10, 15));
 console.log("\n");
+
+
+//This is an example of a complex object and a function to manupulate it 
+console.log("This is an example of a complex object");
+// Setup
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  if (value === "") {
+    delete records[id][prop];
+  } else if (prop !== "tracks" && value !== "") {
+  records[id][prop] = value;
+} else if (prop === "tracks" && !records[id].hasOwnProperty("tracks")) {
+  records[id]["tracks"] = [value];
+} else if (prop === "tracks" && value !== "") {
+  records[id]["tracks"].push(value);
+}
+  return records;
+}
+
+updateRecords(recordCollection, 5439, 'tracks', 'Dancing Queen');
+updateRecords(recordCollection, 5439, 'tracks', 'Some other song');
+console.log(recordCollection );
